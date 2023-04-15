b5cd11d32e114afe9283aab8386ad19837e7c268
fra.scavone <fra.scavone88@gmail.com>
2023-04-15 11:33:11 +0200

    changed baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  projectId: "kyw3ef",
+  projectId: 'kyw3ef',
 
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:5173",
+    baseUrl: 'https://user-onboarding-frascavone.netlify.app/',
   },
 
   component: {
     devServer: {
-      framework: "react",
-      bundler: "vite",
+      framework: 'react',
+      bundler: 'vite',
     },
   },
 });
